60237ed5a112703ec0047eaff3bea9fc4b05d173
ramya <ramya.ravi@opsmx.io>
2021-08-05 00:01:45 +0530

    modifed 3.9 script

--- scripts/oes-data-migration-scripts/migration_v3.8.x_to_v3.9.py
+++ scripts/oes-data-migration-scripts/migration_v3.8.x_to_v3.9.py
@@ -6,18 +6,23 @@ import string
 import random
 import requests
 
+
 def perform_migration(input_string):
     try:
-        print ('Migrating from v3.8.x to v3.9')
+        print('Migrating from v3.8.x to v3.9')
+
+        print("Migrating UserGroups to map as single group and to lowercase")
+        usergroups = fetch_usergroups_data()
+        migration_usergroups_data(usergroups)
 
         # Modify existing user_group table
         modifyUserGroupTable()
 
         # add admin groups configuration to table
-        print ('Printing all admin group names given:: ', input_string)
+        print('Printing all admin group names given:: ', input_string)
         admin_groups = input_string.split(',')
         for group_name in admin_groups:
-            map_user_group_admin(group_name)
+            map_user_group_admin(group_name.lower())
         print("Successfully configured admin groups to table to v3.9")
 
         print("Migrating approval gate parameter entity")
@@ -48,16 +53,77 @@ def perform_migration(input_string):
         platform_conn.rollback()
         visibility_conn.rollback()
         oesdb_conn.rollback()
-        print ('Exception occured during migration : ', e)
+        print('Exception occured during migration : ', e)
     finally:
         platform_conn.close()
         visibility_conn.close()
         oesdb_conn.close()
 
+
+def fetch_usergroups_data():
+    try:
+        cur = platform_conn.cursor()
+        cur.execute("select id,name from user_group")
+        return cur.fetchall()
+    except Exception as e:
+        print("Exception occurred while fetching user_group data : ", e)
+        raise e
+
+
+def migration_usergroups_data(usergroups):
+    print("Total usergroups: ", usergroups)
+    user_groups_map = {usergroup[0]: usergroup[1] for usergroup in usergroups}
+    for usergroup in usergroups:
+        user_group_name = usergroup[1].lower()
+        if (user_group_name in user_groups_map.values()) and (user_group_name != usergroup[1]):
+            user_group_id = list(user_groups_map.keys())[list(user_groups_map.values()).index(user_group_name)]
+            update_usergroup_permission_data(usergroup[0], user_group_id)
+            delete_old_user_group_data(usergroup[0])
+        else:
+            update_usergroup_name_tolower(usergroup[0], user_group_name)
+
+
+def update_usergroup_permission_data(old_user_group_id, new_user_group_id):
+    try:
+        cur = platform_conn.cursor()
+        cur.execute("select count(*) from user_group_permission where group_id =" + str(old_user_group_id))
+        result = cur.fetchone()[0]
+        print("Total usergroups permission for user_group_id: count: ", old_user_group_id, result)
+        if result > 0:
+            cur.execute(
+                "update user_group_permission set group_id = " + str(new_user_group_id) + " where group_id =" + str(
+                    old_user_group_id))
+    except Exception as e:
+        print("Exception occurred while updating data from user_group_permission table : ", e)
+        raise e
+
+
+def delete_old_user_group_data(old_user_group_id):
+    try:
+        cur = platform_conn.cursor()
+        cur.execute("delete from user_group where id =" + str(old_user_group_id))
+    except Exception as e:
+        print("Exception occurred while deleting data from user_group table : ", e)
+        raise e
+
+
+def update_usergroup_name_tolower(user_group_id, user_group_name):
+    try:
+        cur = platform_conn.cursor()
+        cur.execute("select count(*) from user_group where name='" + user_group_name + "'")
+        result = cur.fetchone()[0]
+        if result == 0:
+            cur.execute('UPDATE user_group SET name =' + str(user_group_name) + ' WHERE id =' + str(user_group_id))
+    except Exception as e:
+        print('Exception occurred while updating user_group table name to lowercase: ', e)
+        raise e
+
+
 def alter_approval_gate_parameter():
     try:
         cur = visibility_conn.cursor()
-        cur.execute("ALTER TABLE approval_gate_parameter ADD COLUMN IF NOT EXISTS param_group_id INT NOT NULL DEFAULT 0")
+        cur.execute(
+            "ALTER TABLE approval_gate_parameter ADD COLUMN IF NOT EXISTS param_group_id INT NOT NULL DEFAULT 0")
     except Exception as e:
         print("Exception occured while altering the approval_gate_parameter table : ", e)
         raise e
@@ -66,7 +132,8 @@ def alter_approval_gate_parameter():
 def fetch_distinct_gate_instances():
     try:
         cur = visibility_conn.cursor()
-        cur.execute("select distinct(approval_gate_instance_id) from approval_gate_parameter ORDER BY approval_gate_instance_id")
+        cur.execute(
+            "select distinct(approval_gate_instance_id) from approval_gate_parameter ORDER BY approval_gate_instance_id")
         return cur.fetchall()
     except Exception as e:
         print("Exception occured while fetching distinct gate instances : ", e)
@@ -76,7 +143,9 @@ def fetch_distinct_gate_instances():
 def fetch_distinct_connectors(gate_instance_id):
     try:
         cur = visibility_conn.cursor()
-        cur.execute("select distinct(connector_type) from approval_gate_parameter where approval_gate_instance_id = "+str(gate_instance_id))
+        cur.execute(
+            "select distinct(connector_type) from approval_gate_parameter where approval_gate_instance_id = " + str(
+                gate_instance_id))
         return cur.fetchall()
     except Exception as e:
         print("Exception occured while fetching distinct connectors : ", e)
@@ -86,7 +155,9 @@ def fetch_distinct_connectors(gate_instance_id):
 def update_param_group_id(param_group_id, connector_type, approval_gate_instance_id):
     try:
         cur = visibility_conn.cursor()
-        cur.execute("update approval_gate_parameter set param_group_id = " + str(param_group_id) + " where connector_type = '" + str(connector_type) + "' AND approval_gate_instance_id = "+str(approval_gate_instance_id))
+        cur.execute("update approval_gate_parameter set param_group_id = " + str(
+            param_group_id) + " where connector_type = '" + str(
+            connector_type) + "' AND approval_gate_instance_id = " + str(approval_gate_instance_id))
     except Exception as e:
         print("Exception occured while updating param group id : ", e)
         raise e
@@ -108,10 +179,10 @@ def migrate_pipeline_json(id, pipeline_json):
         pipe_json = json.loads(pipeline_json)
         if len(pipe_json) > 0:
             stages = pipe_json['stages']
-            if stages is not None and len(stages)>0:
+            if stages is not None and len(stages) > 0:
                 for stage in stages:
                     gate_type = stage['type']
-                    if gate_type is not None and len(gate_type)>0:
+                    if gate_type is not None and len(gate_type) > 0:
                         if str(gate_type).strip() == 'Visibility Approval':
                             migrate_approval_gate(stage)
                         elif str(gate_type).strip() == 'Verification':
@@ -121,7 +192,8 @@ def migrate_pipeline_json(id, pipeline_json):
                         elif str(gate_type).strip() == 'Policy Stage':
                             migrate_policy_gate(stage)
                 pipe_json['stages'] = stages
-                cur.execute("update pipeline set pipeline_json = '"+str(json.dumps(pipe_json))+"' where id="+str(id))
+                cur.execute(
+                    "update pipeline set pipeline_json = '" + str(json.dumps(pipe_json)) + "' where id=" + str(id))
 
     except Exception as e:
         print("Exception occured while migrating pipeline json : ", e)
@@ -168,7 +240,7 @@ def migrate_approval_gate(stage):
             gateurl = parameters['gateurl']
 
             connectors = []
-            if gateurl is not None and len(gateurl)>0:
+            if gateurl is not None and len(gateurl) > 0:
                 url_components = str(gateurl).split("/")
                 approval_gate_id = url_components[len(url_components) - 2]
                 configured_tool_connectors = fetch_configured_tool_connectors(approval_gate_id)
@@ -268,7 +340,8 @@ def configure_jira(configured_tool_connector, jiraid):
 
 def fetch_configured_tool_connectors(approval_gate_id):
     try:
-        url = oes_visibility_url + "/visibilityservice/v1/approvalGates/"+str(approval_gate_id)+"/configuredtoolConnectors"
+        url = oes_visibility_url + "/visibilityservice/v1/approvalGates/" + str(
+            approval_gate_id) + "/configuredtoolConnectors"
         headers = {'x-spinnaker-user': oes_admin_user}
         response = requests.get(url=url, headers=headers)
         return json.loads(response.content)
@@ -276,14 +349,15 @@ def fetch_configured_tool_connectors(approval_gate_id):
         print("Exception occured while fetching configured tool connectors : ", e)
         raise e
 
+
 def modifyUserGroupTable():
     try:
         cur = platform_conn.cursor()
         cur.execute("""ALTER TABLE user_group ADD COLUMN IF NOT EXISTS "is_admin" BOOLEAN NOT NULL DEFAULT FALSE"""
                     )
     except Exception as e:
-        print ('Exception occured while modifying user_group table: ',
-               e)
+        print('Exception occured while modifying user_group table: ',
+              e)
         raise e
 
 
@@ -295,24 +369,27 @@ def map_user_group_admin(group_name):
         user_group_id = cur.fetchone()
         update_user_group(user_group_id)
     except Exception as e:
-        print ('Exception occured while fetching data from user_group table: '
-               , e)
+        print('Exception occured while fetching data from user_group table: '
+              , e)
         raise e
 
 
 def update_user_group(user_group_id):
     try:
         cur = platform_conn.cursor()
-        cur.execute('UPDATE user_group SET is_admin = true WHERE id = '+str(user_group_id[0]))
+        cur.execute('UPDATE user_group SET is_admin = true WHERE id = ' + str(user_group_id[0]))
     except Exception as e:
-        print ('Exception occured while updating user_group table: ', e)
+        print('Exception occured while updating user_group table: ', e)
         raise e
+
 
 def alter_cloud_configuration():
     try:
         cur = oesdb_conn.cursor()
-        cur.execute("ALTER TABLE cloud_configuration ALTER COLUMN bakery_secret_key TYPE varchar(10000) USING bakery_secret_key::varchar")
-        cur.execute("ALTER TABLE cloud_configuration ALTER COLUMN secret_key TYPE varchar(10000) USING secret_key::varchar")
+        cur.execute(
+            "ALTER TABLE cloud_configuration ALTER COLUMN bakery_secret_key TYPE varchar(10000) USING bakery_secret_key::varchar")
+        cur.execute(
+            "ALTER TABLE cloud_configuration ALTER COLUMN secret_key TYPE varchar(10000) USING secret_key::varchar")
     except Exception as e:
         print("Exception occured while altering the cloud_configuration table : ", e)
         raise e
@@ -321,14 +398,15 @@ def alter_cloud_configuration():
 if __name__ == '__main__':
     n = len(sys.argv)
     if n != 10:
-        print ('Please pass valid 9 arguments <platform-db-name> <platform-db-host> <visibility-db-name> <visibility-db-host> <sapor-db-name> <sapor-db-host> <oes-visibility-url> <db-port> <oes-admin-user>')
+        print(
+            'Please pass valid 9 arguments <platform-db-name> <platform-db-host> <visibility-db-name> <visibility-db-host> <sapor-db-name> <sapor-db-host> <oes-visibility-url> <db-port> <oes-admin-user>')
 
     platform_db = sys.argv[1]
     platform_host = sys.argv[2]
     visibility_db = sys.argv[3]
     visibility_host = sys.argv[4]
     oes_db = sys.argv[5]
-    oes_host = sys.argv[6]    
+    oes_host = sys.argv[6]
     oes_visibility_url = sys.argv[7]
     port = sys.argv[8]
     oes_admin_user = sys.argv[9]
@@ -336,25 +414,25 @@ if __name__ == '__main__':
     # Establishing the platform db connection
 
     platform_conn = psycopg2.connect(database=platform_db,
-            user='postgres', password='networks123',
-            host=platform_host, port=port)
-    print ('Opened platform database connection successfully')
+                                     user='postgres', password='networks123',
+                                     host=platform_host, port=port)
+    print('Opened platform database connection successfully')
 
-    visibility_conn = psycopg2.connect(database=visibility_db, user='postgres', password='networks123', host=visibility_host, port=port)
+    visibility_conn = psycopg2.connect(database=visibility_db, user='postgres', password='networks123',
+                                       host=visibility_host, port=port)
     print("Visibility database connection established successfully")
-    
-    
+
     # Establishing the sapor db connection
 
     oesdb_conn = psycopg2.connect(database=oes_db,
-            user='postgres', password='networks123',
-            host=oes_host, port=port)
-    print ('Opened sapor database connection successfully')
-    
+                                  user='postgres', password='networks123',
+                                  host=oes_host, port=port)
+    print('Opened sapor database connection successfully')
+
     # Getting input of admin groups
 
     input_string = \
         input('Enter admin groups to be configure in separated by comma:: '
               )
     perform_migration(input_string)
-    
+
